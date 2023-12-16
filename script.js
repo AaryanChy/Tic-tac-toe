74ce6e69a759864cee7db0345a41cc1142554742
Aaryan Chaudhary <aaryanchaudhary188@gmail.com>
2023-12-17 10:14:21 +1100

    Added eventlisteners to each square

--- script.js
+++ script.js
@@ -15,6 +15,46 @@ const GameboardIIFE = (() => {
       boardHTML += `<div class="square" id="square=${index}">${square}</div>`;
     });
     display.innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((eachBox) => {
+      eachBox.addEventListener("click", Game.handleClick);
+    });
   };
   return { render, update };
+})();
+
+const Game = (() => {
+  let players = [];
+  let currentPlayer;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(firstPlayer.value, "X"),
+      createPlayer(secondPlayer.value, "O"),
+    ];
+    DisplayPlayers.renderPlayers(players);
+    currentPlayer = 0;
+    gameOver = false;
+    GameboardIIFE.render();
+  };
+  const handleClick = (event) => {
+    if (gameOver) {
+      return;
+    }
+    let index = parseInt(event.target.id.split("=")[1]);
+    GameboardIIFE.update(index, players[currentPlayer].mark);
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    if (checkWin(GameboardIIFE.getGameBoard(), players[currentPlayer].marker)) {
+      gameOver = true;
+      MessageController.renderMessage(`${players[currentPlayer].name}, Won!`);
+    } else if (checkTie(GameboardIIFE.getGameBoard())) {
+      gameOver = true;
+      MessageController.renderMessage(`Its a Tie`);
+    }
+    if (GameboardIIFE.getGameBoard()[index] !== "") {
+      return;
+    }
+  };
+  return { start, handleClick, players };
 })();
